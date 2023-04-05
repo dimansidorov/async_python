97cf77725877fc24ac0f109001ecbc9990b10eee
dimansidorov <dimkasidorow@gmail.com>
2023-04-05 10:46:35 +0200

    solve 5-6 problems

--- lesson_1/5.py
+++ lesson_1/5.py
@@ -2,3 +2,15 @@
 Выполнить пинг веб-ресурсов yandex.ru, youtube.com и преобразовать результаты
 из байтовового в строковый тип на кириллице.
 """
+
+import subprocess
+import chardet
+
+args = [('ping', 'yandex.ru'), ('ping', 'youtube.com')]
+for item in args:
+    pings = subprocess.Popen(item, stdout=subprocess.PIPE)
+    for line in pings.stdout:
+        result = chardet.detect(line)
+        line = line.decode(result['encoding']).encode('utf-8')
+        print(line.decode('utf-8'))
+        break
