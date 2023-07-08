82458c5dbb2d568b89c34dbc21577c663571367d
dimansidorov <dimkasidorow@gmail.com>
2023-07-08 12:08:48 +0200

    update decorators.py

--- decorators.py
+++ decorators.py
@@ -3,6 +3,7 @@ import logs.config_server_logs
 import logging
 import sys
 import inspect
+import traceback
 
 
 if 'client' in sys.argv[0]:
@@ -17,7 +18,7 @@ def log(func):
         LOGGER.debug(f'Calling function {func.__name__}; '
                      f'Parameters: {args}, {", ".join([f"{k}={v}" for k, v in kwargs.items()])}; '
                      f'Module: {func.__module__}; '
-                     f'Call from function {inspect.stack()[1][3]}')
+                     f'Call from function {traceback.format_stack()[0].strip().split()[-1]}', stacklevel=2)
         return result
 
     return wrapper
