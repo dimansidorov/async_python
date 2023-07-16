c218254157fe21394b55f65fa346b807977b02b6
LENOVO <dimansidorov>
2023-07-16 03:07:04 +0300

    update launcher.py

--- launcher.py
+++ launcher.py
@@ -1,68 +1,27 @@
-import os
-import subprocess
-import time
+"""Программа-лаунчер"""
 
-PROCESS = []
+import subprocess
+
+PROCESSES = []
 
 while True:
     ACTION = input('Выберите действие: q - выход, '
-                   's - запустить сервер и клиенты, x - закрыть все окна: ')
+                   's - запустить сервер и клиенты, '
+                   'x - закрыть все окна: ')
 
     if ACTION == 'q':
         break
     elif ACTION == 's':
-
-        PROCESS.append(subprocess.Popen([
-            'gnome-terminal',
-            "--disable-factory",
-            "--",
-            "python",
-            "./server.py",
-        ])
-        )
-
-        time.sleep(0.1)
-
-        PROCESS.append(subprocess.Popen([
-            'gnome-terminal',
-            "--disable-factory",
-            "--",
-            "python",
-            "./client.py",
-            "-n",
-            "test1",
-
-        ])
-        )
-        time.sleep(0.1)
-
-        PROCESS.append(subprocess.Popen([
-            'gnome-terminal',
-            "--disable-factory",
-            "--",
-            "python",
-            "./client.py",
-            "-n",
-            "test2",
-
-        ])
-        )
-
-        time.sleep(0.1)
-
-        PROCESS.append(subprocess.Popen([
-            'gnome-terminal',
-            "--disable-factory",
-            "--",
-            "python",
-            "./client.py",
-            "-n",
-            "test3",
-        ])
-        )
-
-        time.sleep(0.1)
+        PROCESSES.append(subprocess.Popen('python server.py',
+                                          creationflags=subprocess.CREATE_NEW_CONSOLE))
+        PROCESSES.append(subprocess.Popen('python client.py -n test1',
+                                          creationflags=subprocess.CREATE_NEW_CONSOLE))
+        PROCESSES.append(subprocess.Popen('python client.py -n test2',
+                                          creationflags=subprocess.CREATE_NEW_CONSOLE))
+        PROCESSES.append(subprocess.Popen('python client.py -n test3',
+                                          creationflags=subprocess.CREATE_NEW_CONSOLE))
     elif ACTION == 'x':
-        while PROCESS:
-            VICTIM = PROCESS.pop()
+        while PROCESSES:
+            VICTIM = PROCESSES.pop()
             VICTIM.kill()
+
